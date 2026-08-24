510da15f083e6b451c15f66ffc9cdf177fa8a01e
hsb4225 <74960635+hsb4225@users.noreply.github.com>
2026-08-24 22:12:22 +0900

    [Passed] 배열 채우기 (138ms, 10MB)

---
+++ trail1/배열 채우기/filling-array.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        int cnt = 0;
+        for(int i = 0; i < 10; i++) {
+            int n = sc.nextInt();
+            if(n == 0) {
+                break;
+            }
+            arr[i] = n;
+            cnt++;
+        }
+        for(int i = cnt - 1; i >= 0; i--) {
+            System.out.printf("%d " , arr[i]);
+        }
+    }
+}
