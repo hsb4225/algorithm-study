a51c25f38d956fe27bc4fef700ed519f6fe9489e
hsb4225 <74960635+hsb4225@users.noreply.github.com>
2026-08-24 21:26:08 +0900

    [Passed] 학점 계산기 (147ms, 12MB)

---
+++ trail1/학점 계산기/credit-calculator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        double[] arr = new double[n];
+
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextDouble();
+        }
+        double sum = 0;
+        double avg = 0;
+        for(double a : arr) {
+            sum += a;
+        }
+        avg = sum / n;
+        System.out.printf("%.1f\n", avg);
+        if(avg >= 4.0) {
+            System.out.print("Perfect");
+        } else if(avg >= 3.0) {
+            System.out.print("Good");
+        } else {
+            System.out.print("Poor");
+        }
+    }
+}
