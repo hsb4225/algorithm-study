f9d8c059ae0478f540ff9ff72baa6c94b70dbf3f
hsb4225 <74960635+hsb4225@users.noreply.github.com>
2026-09-10 23:05:56 +0900

    [Passed] 일의 자리 배열 (137ms, 10MB)

---
+++ trail1/일의 자리 배열/array-with-units.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        for(int i = 0; i < 2; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int i = 2; i < 10; i++) {
+            arr[i] = arr[i - 1] + arr[i - 2];
+            if(arr[i] >= 10) {
+                arr[i] -= 10;
+            }
+        }
+        for(int i = 0; i < 10; i++) {
+            System.out.printf("%d ", arr[i]);
+        }
+    }
+}
